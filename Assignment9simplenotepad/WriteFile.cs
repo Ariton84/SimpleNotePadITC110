c1e0ad5e401af089804f13cfa4474447a267e956
Ariton84 <vanver@gmail.com>
2016-11-29 12:02:37 -0800

    Assignment9
    Simple notepad assignment

---
+++ Assignment9simplenotepad/WriteFile.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Assignment9simplenotepad
+{
+    class WriteFile
+    {
+        private StreamWriter writer;
+        string filepath = null;
+
+        public WriteFile(string path)
+        {
+            try
+            {
+                filepath = path;
+                writer = new StreamWriter(path, true);
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void AddText(string line)
+        {
+            writer.Write(line);
+        }
+
+        public void CloseFile()
+        {
+            writer.Close();
+        }
+
+
+    }//end class
+}//end namespace
